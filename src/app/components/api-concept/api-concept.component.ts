85113aad9668e0fa18c1e0a28c4865627813f3bd
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2023-01-04 15:04:57 +0000

    Add environment variable and call API

--- src/app/components/api-concept/api-concept.component.ts
+++ src/app/components/api-concept/api-concept.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ajax } from 'rxjs/ajax';
+import { map, catchError, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+interface Data {
+  login?: string;
+  id?: number;
+  node_id?: string;
+  avatar_url?: string;
+  gravatar_id?: string;
+  url?: string
+  html_url?: string;
+  followers_url?: string;
+  following_url?: string;
+  gists_url?: string;
+  starred_url?: string;
+  subscriptions_url?: string;
+  organizations_url?: string;
+  repos_url?: string;
+  events_url?: string;
+  received_events_url?: string;
+  type?: string;
+  site_admin?: boolean;
+}
 
 @Component({
   selector: 'app-api-concept',
@@ -7,9 +31,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApiConceptComponent implements OnInit {
 
+  public data?: Data[]
+
   constructor() { }
 
   ngOnInit(): void {
+    const obs$ = ajax(`${environment.env_URL}/users?per_page=5`)
+    obs$.subscribe({
+      next: value => {
+        this.data = value.response as Data[];
+        console.log('this.data', this.data)
+      },
+      error: err => console.log(err)
+    });
   }
 
 }
